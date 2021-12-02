f1efdd0d28300eea338b10b3d9e06ea1fed953a2
Matt Lander <matthew.lander@emperor.works>
2021-12-02 14:45:52 +0000

    day 2 of advent of code 2021

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { readFile } from "fs/promises"
+
+type offset = {dist:number,depth:number}
+
+const day2A = ()=>{
+ readFile('./day2.txt').then(buffer=>{
+    const lines = buffer.toString().split(/\r?\n/)
+    const result = lines.reduce<offset>((p:offset,c:string)=>{
+      const parts = c.split(' ')
+      const val = Number.parseInt(parts[1])
+      switch (parts[0]){
+        case "forward":
+          p.dist += val
+          break
+        case "down":
+          p.depth += val
+          break
+        case "up":
+          p.depth -= val
+          break
+      }
+      return p;
+    }, {dist:0, depth:0})
+    console.log(result.depth * result.dist)
+ })
+}
+
+type aimOffset = {aim:number, dist:number,depth:number}
+const day2B = ()=>{
+  readFile('./day2.txt').then(buffer=>{
+     const lines = buffer.toString().split(/\r?\n/)
+     const result = lines.reduce<aimOffset>((p:aimOffset,c:string)=>{
+       const parts = c.split(' ')
+       const val = Number.parseInt(parts[1])
+       switch (parts[0]){
+         case "forward":
+           p.dist += val
+           p.depth += val * p.aim
+           break
+         case "down":
+           p.aim += val
+           break
+         case "up":
+           p.aim -= val
+           break
+       }
+       return p;
+     }, {aim:0, dist:0, depth:0})
+
+     console.log(result.depth * result.dist)
+  })
+ }
+
+day2B()
