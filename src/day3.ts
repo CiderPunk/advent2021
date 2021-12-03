8cf65cfba13f5d8eead291405e32965f5f90bc00
Matt Lander <matt@ciderpunk.net>
2021-12-03 13:31:24 +0000

    day 3!

---
+++ src/day3.ts
@@ -0,0 +1,49 @@
+import { readFile } from "fs/promises"
+
+export const day3A = async ():Promise<number>=>{
+ return readFile('./day3.txt').then(buffer=>{
+    const lines = buffer.toString().split(/\r?\n/)
+    const result = lines.reduce<Array<number>>((p:Array<number>,c:string)=>{
+      for (let i = 0; i < c.length; i++ ){
+        if (p[i] === undefined){ 
+          p[i] = 0
+        }
+        p[i] += parseInt(c[i])
+      }
+      return p
+    }, new Array<number>())
+    let gamma = ""
+    let epsilon = ""
+    for (let i = 0; i < result.length; i++ )
+    {
+      const bit = result[i] > (lines.length / 2)
+      gamma +=  bit ? '1' : '0'
+      epsilon += bit ? '0' : '1'
+    }
+    return parseInt(gamma, 2) * parseInt(epsilon,2)
+ })
+}
+
+
+export const day3B = async ():Promise<number>=>{
+  return readFile('./day3.txt').then(buffer=>{
+    const lines = buffer.toString().split(/\r?\n/)
+    //copy/filter out blank line
+    let ox = lines.filter(l=>l.length > 0)
+    let cs = lines.filter(l=>l.length > 0)
+    for (let i = 0; i < 12; i++){
+      if (ox.length > 1){
+        const countOx  = ox.reduce<number>((p:number,c:string)=>p + parseInt(c[i]) , 0)
+        var filterOx = countOx >= (ox.length / 2) ? "1" : "0";
+        ox = ox.filter(p=>{ return p[i] == filterOx })
+      }
+      if (cs.length > 1){
+        const countCs  = cs.reduce<number>((p:number,c)=>{ return p + parseInt(c[i]) }, 0)
+        var filterCs = countCs < (cs.length / 2) ? "1" : "0";
+        cs = cs.filter(p=>{ return p[i] == filterCs })
+      }
+    }
+    return  parseInt(ox[0], 2) * parseInt(cs[0], 2) 
+  })
+ }
+ 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { day1A, day1B } from "./day1"
 import { day2A, day2B } from "./day2"
+import { day3A, day3B } from "./day3"
 
 day1A().then(val=>{ console.log(`Day 1A: ${val}`) })
 day1B().then(val=>{ console.log(`Day 1B: ${val}`) })
 day2A().then(val=>{ console.log(`Day 2A: ${val}`) })
 day2B().then(val=>{ console.log(`Day 2B: ${val}`) })
+day3A().then(val=>{ console.log(`Day 3A: ${val}`) })
+day3B().then(val=>{ console.log(`Day 3B: ${val}`) })
